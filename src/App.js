709aeeb5a3e72b03baf5c510292f3fd62e386133
kiatklai <likhitthaworn.k@gmail.com>
2023-07-28 21:41:46 +0900

    為替のAPI導入

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import CurrencyComponent from './components/CurrencyComponent';
 import money from './img/money.png'
 function App() {
+  const url = `https://api.exchangerate-api.com/v4/latest/USD`
+  
+  useEffect(()=>{
+    fetch(url).then(res=>res.json())
+    .then(data=>console.log(data))
+  })
   return (
     <div>
       <img src={money} alt="logo" className="money-img"/>
