68569f7068468c302a45b9944a7f02b2e7b37560
kiatklai <likhitthaworn.k@gmail.com>
2023-07-28 20:43:03 +0900

    component作成

---
+++ src/components/CurrencyComponent.js
@@ -0,0 +1,14 @@
+const CurrencyComponent=()=>{
+  return(
+    <div className="currency">
+      <select>
+        <option value="THB">THB</option>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+      </select>
+      <input type="number"/>
+    </div>
+  )
+}
+
+export default CurrencyComponent
